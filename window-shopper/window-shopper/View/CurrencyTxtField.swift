fb87a289506ac13e4fb6d60a3cd0c296b8f6d6a2
Nico Yeager <nicoyeager@gmail.com>
2020-04-06 20:58:31 -0400

    started ui, coded placehldr

---
+++ window-shopper/window-shopper/View/CurrencyTxtField.swift
@@ -0,0 +1,37 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Nico Yeager on 4/6/20.
+//  Copyright © 2020 Nico Yeager. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+    
+    //right now, the second text field type (Item price) is also inheriting from this class. need to see what mark does in the next tutorial.  He may create a different class within a different file to have that text field inherit from.  he may create the class within this swift file and change the name.  not sure what is best practice... yet
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2523009418)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+        
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
