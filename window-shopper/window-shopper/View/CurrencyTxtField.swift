abdd4fecf241eb1618bbedd242eca3bb1c45c948
Nico Yeager <nicoyeager@gmail.com>
2020-04-07 14:51:14 -0400

    used draw with $

--- window-shopper/window-shopper/View/CurrencyTxtField.swift
+++ window-shopper/window-shopper/View/CurrencyTxtField.swift
@@ -11,6 +11,24 @@ import UIKit
 @IBDesignable
 class CurrencyTxtField: UITextField {
     
+    //create subview to show in left corner of textview
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.8592997193, green: 0.8541920781, blue: 0.8632262349, alpha: 0.8023062928)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+        
+        
+    }
+    
     //right now, the second text field type (Item price) is also inheriting from this class. need to see what mark does in the next tutorial.  He may create a different class within a different file to have that text field inherit from.  he may create the class within this swift file and change the name.  not sure what is best practice... yet
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,6 +44,7 @@ class CurrencyTxtField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2523009418)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
         // if placeholder text is not nil, then set color of text to white.
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
