0b12a880099acbe77f9ba317ff16d284c8366b31
Nico Yeager <nicoyeager@gmail.com>
2020-04-07 13:03:29 -0400

    finished lesson2

--- window-shopper/window-shopper/View/CurrencyTxtField.swift
+++ window-shopper/window-shopper/View/CurrencyTxtField.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTxtField: UITextField {
     
     //right now, the second text field type (Item price) is also inheriting from this class. need to see what mark does in the next tutorial.  He may create a different class within a different file to have that text field inherit from.  he may create the class within this swift file and change the name.  not sure what is best practice... yet
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+    }
+    override func prepareForInterfaceBuilder() {
+        // some code here to make the changes be updated in the story board
+        customizeView()
+    }
+    
+    //how we modified/customized the UITextField class to be a subclass called "CurrencyTxtField"
+    func customizeView(){
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2523009418)
         layer.cornerRadius = 5.0
         textAlignment = .center
@@ -22,10 +32,7 @@ class CurrencyTxtField: UITextField {
             attributedPlaceholder = place
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
-        
-        
     }
-
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
